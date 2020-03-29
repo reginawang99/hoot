6a261a72d5edb305eb89148628d3eb2663931734
Neil Prajapati <neilp0101@gmail.com>
2020-03-28 20:21:50 -0700

    no class based react components

--- frontend/caw/src/App.js
+++ frontend/caw/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import LinkPanel from './Panel/LinkPanel';
 import SearchResultsPanel from './SearchResultsPanel';
@@ -20,83 +20,58 @@ import axios from "axios";
 *
 */
 
-class App extends React.Component {
+function App () {
+  const [query, setQuery] = useState("")
 
-  state = {
-      searchResults: null,
-      query_params: {
-        query: "",
-        sections: [], //[1,2,...]
-        tags: [] //
-      },
-      sections: [], // [{id: 1, name: blah},...]
-      quickLinks: [
-        {"text": "asdf", "link": "http://link.com"},
-        {"text": "another link", "link": "http://link.com"},
-        {"text": "another link", "link": "http://link.com"},
-        {"text": "another link", "link": "http://link.com"},
-      ]
-    }
-
-  componentDidMount (){
-    //STATE
-    
-  }
-
-  updateQuery = (e) => {
-    this.setState({query_params: {
-      ...this.state.query_params,
-      query: e.target.value,
-    }});
-  };
-
-  
-
-
-  render() {
-    
-    return (
-      <Router>
-        <div className="App">
-          <div className="header"> 
-            <h1 className="header-text">DAILY BRUIN STYLE GUIDE</h1>
-            <div>
-              <input className="header-search-input" type="text" placeholder="search here" value={this.state.query_params.query} onChange={this.updateQuery} />
-              <Link className="header-search-button" to={`/search/all/${this.state.query_params.query}`}>S</Link>
-            </div>
-          </div>
-          <div className="caw-body">
-            <div className="search-result-container">
-              
-              <Route exact={true} path="/entry/:entryName" component={SingleSearchResultView} />
-              <Route exact={true} path="/search/:sections/:query" component={SearchResultsPanel}/>
-              
-            </div>
-
-            <div class="link-sidebar">
-              <LinkPanel header="Sections" body={
-                [
-                  {"text": "Design", "link": "http://link.com"},
-                  {"text": "Arts", "link": "http://link.com"},
-                  {"text": "News", "link": "http://link.com"},
-                  {"text": "Opinion", "link": "http://link.com"},
-                  {"text": "Sports", "link": "http://link.com"},
-                ]
-              }/>
-              <LinkPanel header="Quick Links" body={this.state.quickLinks}/>
-              <LinkPanel header="Guides" body={
-                [
-                  {"text": "Link1", "link": "http://link.com"},
-                  {"text": "Guide link", "link": "http://link.com"}
-                ]
-              }/>
-
-            </div>
+  return (
+    <Router>
+      <div className="App">
+        <div className="header"> 
+          <h1 className="header-text">DAILY BRUIN STYLE GUIDE</h1>
+          <div>
+            <input className="header-search-input" type="text" placeholder="search here" value={query} onChange={(e) => setQuery(e.target.value)} />
+            <Link className="header-search-button" to={`/search/all/${query}`}>S</Link>
           </div>
         </div>
-      </Router>
-    );
-  }
+        <div className="caw-body">
+          <div className="search-result-container">
+            
+            <Route exact={true} path="/entry/:entryName" component={SingleSearchResultView} />
+            <Route exact={true} path="/search/:sections/:query" component={SearchResultsPanel}/>
+            
+          </div>
+
+          <div className="link-sidebar">
+            <LinkPanel header="Sections" body={
+              [
+                {"text": "Design", "link": "http://link.com"},
+                {"text": "Arts", "link": "http://link.com"},
+                {"text": "News", "link": "http://link.com"},
+                {"text": "Opinion", "link": "http://link.com"},
+                {"text": "Sports", "link": "http://link.com"},
+              ]
+            }/>
+            <LinkPanel header="Quick Links" body={
+              [
+                {"text": "asdf", "link": "http://link.com"},
+                {"text": "another link", "link": "http://link.com"},
+                {"text": "another link", "link": "http://link.com"},
+                {"text": "another link", "link": "http://link.com"},
+              ]
+            }/>
+            <LinkPanel header="Guides" body={
+              [
+                {"text": "Link1", "link": "http://link.com"},
+                {"text": "Guide link", "link": "http://link.com"}
+              ]
+            }/>
+
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
 }
+
 
 export default App;
