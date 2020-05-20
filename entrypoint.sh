cfadd16333e26efb32f506f5f56927ff25e11a92
wenhong <lamwenhong@yahoo.com.sg>
2020-05-20 18:37:30 +0800

    postgres script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 set -e #stops execution of script if failure occurs somewhere
 
+python wait_for_postgres.py
+
 ./hoot/manage.py collectstatic --noinput
 ./hoot/manage.py migrate
 ./hoot/manage.py loaddata spreadsheet_data_fixture.json
 echo "Collected, migrated and loaded."
 
 cd hoot
-gunicorn --bind 0.0.0.0:5000 --access-logfile - hoot.wsgi:application --daemon #change django_project to folder name
+gunicorn --bind 0.0.0.0:5000 --access-logfile - hoot.wsgi:application --daemon -R #change django_project to folder name
 cd ..
 echo "Backend initialized."
 
